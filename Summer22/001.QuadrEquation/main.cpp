79844400618d12c2447de1ae9a637084cabc530c
Максимилиан <max4tim@bk.ru>
2022-08-23 19:00:55 +0300

    Summer22 001.QuadrEquation IO to own file

--- Summer22/001.QuadrEquation/main.cpp
+++ Summer22/001.QuadrEquation/main.cpp
@@ -1,36 +1,11 @@
-#include <stdio.h>
-#include <cassert>
-
-#include "quadrEquation.h"
+#include "quadrEquationIO.h"
 
 int main() {
     QuadrEquation equation = {};
-    printf("Print coefficients for \"a*x^2 + b*x + c = 0\" equation: \n");
-    while (!quadrEquation::scan(&equation)) {
-        while (getchar() != '\n');
-        printf("Input is incorrect! Print coefficients for \"a*x^2 + b*x + c = 0\" equation: \n");
-    }
 
+    inputQuadrEquation(&equation);
     quadrEquation::print(equation);
-
-    double root1 = 0;
-    double root2 = 0;
-    switch (quadrEquation::solve(&equation, &root1, &root2)) {
-    case quadrEquation::NumberOfRoots::NO_ROOTS:
-        printf("No roots\n");
-        break;
-    case quadrEquation::NumberOfRoots::ONE_ROOT:
-        printf("Root: %lg\n", root1);
-        break;
-    case quadrEquation::NumberOfRoots::TWO_ROOTS:
-        printf("Roots: %lg, %lg\n", root1, root2);
-        break;
-    case quadrEquation::NumberOfRoots::INF_ROOTS:
-        printf("Any number is a root\n");
-        break;
-    default:
-        assert(false && "quadrEquation()'s return is not a NumberOfRoots'es member");
-    }
+    outputQuadrEquation(&equation);
 
     return 0;
 }
---
+++ Summer22/001.QuadrEquation/quadrEquationIO.h
@@ -0,0 +1,4 @@
+#include "QuadrEquation.h"
+
+void inputQuadrEquation(QuadrEquation* equationPtr);
+void outputQuadrEquation(QuadrEquation* equationPtr);
