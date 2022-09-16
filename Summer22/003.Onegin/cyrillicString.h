ba8a328ae729d9220c491e8ef48f81159bc22b7e
Максимилиан <max4tim@bk.ru>
2022-09-16 15:16:29 +0300

    Summer22 003.Onegin cyrillicString module
    Good version with good code

---
+++ Summer22/003.Onegin/cyrillicString.h
@@ -0,0 +1,45 @@
+/**
+ * @file CyrillicString.h
+ * @author Maximilian (https://github.com/Maximilian04)
+ * @brief Functions for comparing cyrillic strings
+ * @date 2022-09-16
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef CYRILLICSTRING_H
+#define CYRILLICSTRING_H
+
+#include "ListOfLines.h"
+
+namespace cyrillicString {
+    extern const ListOfLines* linesOfAlphabet;
+
+    enum CmpCyrillicResult {
+        BAD_A,
+        BAD_B,
+        BAD_BOTH,
+        LESSER,
+        GREATER,
+        EQV,
+    };
+
+    enum {
+        BAD_CHAR = -1,
+    };
+
+    void setAlphabet(const ListOfLines* linesOfAlphabet);
+
+    int getCyrillicIndex(const char a);
+    CmpCyrillicResult cmpCyrillic(const char a, const char b);
+    int cmpLinesStr(const char* a, const char* b);
+    int cmpLinesBackStr(const char* a, const char* b);
+    int cmpLines(const void* a, const void* b);
+    int cmpLinesBack(const void* a, const void* b);
+    int cmpLinesRealNum(const void* a, const void* b);
+}
+
+void uq_sort(void* arr, int begin, int end, int(*cmp)(const void*, const void*), int size);
+
+#endif // CYRILLICSTRING_H
--- Summer22/003.Onegin/pervert.cpp
+++ Summer22/003.Onegin/pervert.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include <assert.h>
+
+#include "cyrillicString.h"
 
 #include "pervert.h"
 
@@ -10,170 +10,19 @@ const char FILE_ALPH_NAME[] = "alphabet.txt";
 const char FILE_OUT_NAME[] = "build\\PunyEvgeniyOnegin.txt";
 
 namespace pervert {
+    int cmpLinesRealNum(const void* a, const void* b); ///< Compare via comparing pointers
+
     ListOfLines linesOfPoem     = { nullptr, nullptr, 0 };
     ListOfLines linesOfAlphabet = { nullptr, nullptr, 0 };
     FILE* outFile = nullptr; ///< File for output
 
-    enum CmpCyrillicResult {
-        BAD_A,
-        BAD_B,
-        BAD_BOTH,
-        LESSER,
-        GREATER,
-        EQV,
-    };
-
-    int getCyrillicIndex(const char a);
-    CmpCyrillicResult cmpCyrillic(const char a, const char b);
-    int cmpLinesStr(const char* a, const char* b);
-    int cmpLinesBackStr(const char* a, const char* b);
-    int cmpLinesRealNum(const char* a, const char* b);
-    int cmpLines(const void* a, const void* b);
-    int cmpLinesBack(const void* a, const void* b);
-    int cmpLinesRealNum(const void* a, const void* b);
-    void uq_sort(void* arr, int size, int begin, int end, int(*cmp)(const void*, const void*));
-
-    enum {
-        BAD_CHAR = -1,
-    };
-
-    int getCyrillicIndex(const char a) {
-        for (int i = 0; i < linesOfAlphabet.size; ++i) {
-            if (
-                a == linesOfAlphabet.lines[i].str[0]
-                )
-                return i;
-        }
-
-        return BAD_CHAR;
-    }
-
-    CmpCyrillicResult cmpCyrillic(const char a, const char b) {
-        int aIndex = getCyrillicIndex(a);
-        int bIndex = getCyrillicIndex(b);
-
-        if (aIndex == BAD_CHAR && bIndex != BAD_CHAR) {
-            return CmpCyrillicResult::BAD_A;
-        }
-        if (aIndex != BAD_CHAR && bIndex == BAD_CHAR) {
-            return CmpCyrillicResult::BAD_B;
-        }
-        if (aIndex == BAD_CHAR && bIndex == BAD_CHAR) {
-            return CmpCyrillicResult::BAD_BOTH;
-        }
-        if (aIndex < bIndex) {
-            return CmpCyrillicResult::LESSER;
-        }
-        if (aIndex > bIndex) {
-            return CmpCyrillicResult::GREATER;
-        }
-        return CmpCyrillicResult::EQV;
-    }
-
-    int cmpLinesStr(const char* a, const char* b) {
-        int iA = 0;
-        int iB = 0;
-        while (true) {
-            if (a[iA] == '\0') {
-                if (b[iB] == '\0') {
-                    return 0;
-                } else {
-                    return -1;
-                }
-            } else {
-                if (b[iB] == '\0') {
-                    return 1;
-                } else {
-                    switch (cmpCyrillic(a[iA], b[iB])) {
-                    case CmpCyrillicResult::BAD_A:
-                        ++iA;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::BAD_B:
-                        ++iB;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::BAD_BOTH:
-                        ++iA;
-                        ++iB;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::LESSER:
-                        return -1;
-                        break;
-                    case CmpCyrillicResult::GREATER:
-                        return 1;
-                        break;
-                    case CmpCyrillicResult::EQV:
-                        ++iA;
-                        ++iB;
-                        break;
-                    default:
-                        assert(false);
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    int cmpLinesBackStr(const char* a, const char* b) {
-        int iA = 0;
-        int iB = 0;
-        while (a[iA] != '\0') ++iA;
-        while (b[iB] != '\0') ++iB;
-        while (true) {
-            if (iA == -1) {
-                if (iB == -1) {
-                    return 0;
-                } else {
-                    return -1;
-                }
-            } else {
-                if (iB == -1) {
-                    return 1;
-                } else {
-                    switch (cmpCyrillic(a[iA], b[iB])) {
-                    case CmpCyrillicResult::BAD_A:
-                        --iA;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::BAD_B:
-                        --iB;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::BAD_BOTH:
-                        --iA;
-                        --iB;
-                        continue;
-                        break;
-                    case CmpCyrillicResult::LESSER:
-                        return -1;
-                        break;
-                    case CmpCyrillicResult::GREATER:
-                        return 1;
-                        break;
-                    case CmpCyrillicResult::EQV:
-                        --iA;
-                        --iB;
-                        break;
-                    default:
-                        assert(false);
-                    }
-                }
-            }
-        }
-
-        return 0;
-    }
-
-    int cmpLines(const void* a, const void* b) {
-        return cmpLinesStr(((const Line*)a)->str, ((const Line*)b)->str);
-    }
-
-    int cmpLinesBack(const void* a, const void* b) {
-        return cmpLinesBackStr(((const Line*)a)->str, ((const Line*)b)->str);
+    /**
+     * @brief Sort lines with its beginning
+     *
+     */
+    void sortLinesBeginning() {
+        //qsort(linesOfPoem.lines, linesOfPoem.size, sizeof(Line), cmpLines);
+        uq_sort(linesOfPoem.lines, 0, linesOfPoem.size - 1, cyrillicString::cmpLines, sizeof(Line));
     }
 
     int cmpLinesRealNum(const void* a, const void* b) {
@@ -184,67 +33,6 @@ namespace pervert {
             return 1;
         }
         return 0;
-    }
-
-    void uq_sort(void* arr, int begin, int end, int(*cmp)(const void*, const void*), int size);
-
-    void uq_sort(void* arr, int begin, int end, int(*cmp)(const void*, const void*), int size) {
-        if (begin >= end) {
-            return;
-        }
-
-        int baseI = (begin + end) / 2;
-
-        int l = begin;
-        int r = end;
-
-        void* tmp = calloc(1, size);
-
-        while (l < r) {
-            while (cmp((char*)arr + l * size, (char*)arr + baseI * size) == -1) ++l;
-            while (cmp((char*)arr + r * size, (char*)arr + baseI * size) == +1) --r;
-
-            if (l >= r) {
-                break;
-            }
-
-            // swap
-            if (l == baseI) {
-                baseI = r;
-            } else if (r == baseI) {
-                baseI = l;
-            }
-            memcpy(tmp,                   (char*)arr + l * size, size);
-            memcpy((char*)arr + l * size, (char*)arr + r * size, size);
-            memcpy((char*)arr + r * size, tmp,                   size);
-            
-            // Ифы имени Миши
-            if (l != baseI)
-                ++l;
-            if (r != baseI)
-                --r;
-        }
-
-        free(tmp);
-
-        for (int i = begin; i <= baseI; ++i) {
-            assert(cmp((char*)arr + i * size, (char*)arr + baseI * size) <= 0 || (printf("%d %d", i, baseI), false));
-        }
-        for (int i = baseI; i <= end; ++i) {
-            assert(cmp((char*)arr + i * size, (char*)arr + baseI * size) >= 0 || (printf("%d %d", i, baseI), false));
-        }
-
-        uq_sort(arr, begin, baseI - 1, cmp, size);
-        uq_sort(arr, baseI + 1,   end, cmp, size);
-    }
-
-    /**
-     * @brief Sort lines with its beginning
-     *
-     */
-    void sortLinesBeginning() {
-        //qsort(linesOfPoem.lines, linesOfPoem.size, sizeof(Line), cmpLines);
-        uq_sort(linesOfPoem.lines, 0, linesOfPoem.size - 1, cmpLines, sizeof(Line));
     }
 
     /**
@@ -262,7 +50,7 @@ namespace pervert {
      */
     void sortLinesEnding() {
         //qsort(linesOfPoem.lines, linesOfPoem.size, sizeof(Line), cmpLinesBack);
-        uq_sort(linesOfPoem.lines, 0, linesOfPoem.size - 1, cmpLinesBack, sizeof(Line));
+        uq_sort(linesOfPoem.lines, 0, linesOfPoem.size - 1, cyrillicString::cmpLinesBack, sizeof(Line));
     }
 
     /**
@@ -289,6 +77,7 @@ namespace pervert {
      */
     void uploadAlphabet() {
         listOfLines::uploadList(&linesOfAlphabet, FILE_ALPH_NAME);
+        cyrillicString::setAlphabet(&linesOfAlphabet);
     }
 
     /**
