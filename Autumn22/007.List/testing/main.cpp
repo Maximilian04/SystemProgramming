f3a26ec1cf117bc1e617d374108512eaeeac9b37
Максимилиан <max4tim@bk.ru>
2022-10-31 17:42:45 +0300

    Autumn22 007.List Testing system

--- Autumn22/007.List/testing/main.cpp
+++ Autumn22/007.List/testing/main.cpp
@@ -1,10 +1,10 @@
-#include "stdio.h"
+#include <stdio.h>
 
+#include "tests.h"
 #include <List.h>
 
-int main() {
-    List::printHW();
-    printf("Program finished 0\n");
+int main(int argc, const char* const* const argv) {
+    testingSystem::main(argc, argv);
 
     return 0;
 }
---
+++ Autumn22/007.List/trun.bat
@@ -0,0 +1,3 @@
+@ cd testing
+@ call runCP
+@ cd ..
