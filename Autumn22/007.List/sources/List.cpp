fce34cface9f4dce86689770d74694909a7dc39b
Максимилиан <max4tim@bk.ru>
2022-11-08 16:13:26 +0300

    Autumn22 007.List pushBack

--- Autumn22/007.List/sources/List.cpp
+++ Autumn22/007.List/sources/List.cpp
@@ -1,13 +1,16 @@
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "List.h"
+
+#define VERIFY(obj) if (verify(obj)) { LIST__dump(*obj)); return Error::VERIFIER_ERR; }
 
 List::Error List::pushBack(List* list, size_t size, void* src) {
     assert(list != nullptr);
     // assert(src != nullptr);
 
-    ListElem* newElem = (ListElem*)calloc(size, 1);
+    ListElem* newElem = (ListElem*)calloc(sizeof(ListElem), 1);
     if (!newElem) return Error::MEM_ERR;
 
     if (list->head) {
@@ -17,6 +20,10 @@ List::Error List::pushBack(List* list, size_t size, void* src) {
         newElem->prev = list->head;
         list->head = newElem;
     }
+
+    list->head->valuePtr = calloc(size, 1);
+    if (!list->head->valuePtr) return Error::MEM_ERR;
+    memcpy(list->head->valuePtr, src, size);
 
     return Error::OK;
 }
