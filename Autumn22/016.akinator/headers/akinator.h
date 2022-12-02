c218dd8c523a649639578c2e498db4094d74b03c
Максимилиан <max4tim@bk.ru>
2022-12-02 12:21:59 +0300

    Autumn22 016.akinator ctor

--- Autumn22/016.akinator/headers/akinator.h
+++ Autumn22/016.akinator/headers/akinator.h
@@ -11,7 +11,22 @@
 #ifndef AKINATOR_H
 #define AKINATOR_H
 
+#include <Tree.h>
+
 class Akinator {
+public:
+    Tree data;
+
+    DebugInfo debugInfo;
+
+    enum Error {
+        OK = 0,      ///< No errors
+        MEM_ERR,     ///< Error in memory allocation
+        NULLPTR_ERR, ///< Nullptr is occured
+        EMPTY,       ///< No elements in tree
+    };
+
+    static Error ctor(Akinator* const akinator, DEBUGINFO_CTOR_ARGS_H);
 };
 
 #endif // AKINATOR_H
--- Autumn22/016.akinator/sources/akinator.cpp
+++ Autumn22/016.akinator/sources/akinator.cpp
@@ -1,3 +1,24 @@
 #include <assert.h>
 
 #include "Akinator.h"
+
+/**
+ * @brief Akinator constructor
+ *
+ * @param [out] akinator Akinator
+ * @return Akinator::Error Error code
+ */
+Akinator::Error Akinator::ctor(Akinator* const akinator, DEBUGINFO_CTOR_ARGS_H) {
+    assert(akinator);
+
+    DEBUGINFO_CTOR_ARGS_INITIALIZE(akinator);
+
+    Tree__ctor(akinator->data, char const*, "%s");
+    // tree->root = nullptr;
+
+    // tree->elemSize = elemSize;
+    // tree->outFunc = outFunc;
+
+    // VERIFY(tree);
+    return Error::OK;
+}
