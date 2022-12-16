0d663b35923835f3e6eb7a6937760bd5c7ac0ded
Максимилиан <max4tim@bk.ru>
2022-12-16 05:24:21 +0300

    Autumn22 016.akinator Demo

--- Autumn22/016.akinator/sources/main.cpp
+++ Autumn22/016.akinator/sources/main.cpp
@@ -3,37 +3,52 @@
 
 #include "Akinator.h"
 
+const size_t INPUT_BUFFER_SIZE = 512;
+
 int main(int argc, const char* const* const argv) {
     Akinator akinator{};
     Akinator__ctor(akinator);
 
-    char const* s = "roooaaapppt";
-    Tree::addRoot(&akinator.data, &s);
-    TreeIterator it{};
-    Tree::set2Root(&akinator.data, &it);
-    s = "leefaaapppt";
-    Tree::addRight(&akinator.data, &it, &s);
-
+    if (Akinator::upload(&akinator, "data.txt")) {
+        printf("upload error\n");
+        return 1;
+    }
+    logger::logMsg(COLORED_TEXT(COLORS_SETTINGS_NOTE_TEXTCOLOR, "begin:"));
     Tree__dump(akinator.data);
+
+
+    TreeIterator position{};
+    Tree::set2Root(&akinator.data, &position);
+
+    size_t bufN = strFParser::addCallocBuf();
+    char input[INPUT_BUFFER_SIZE];
+    while (TreeIterator::getRightPtr(&position)) {
+        printf("%s\n", Tree::getOutFunc(&akinator.data)(bufN, TreeIterator::getValue(&position)));
+        scanf("%s", input);
+
+        if (*input == 'y')
+            TreeIterator::right(&position);
+        else
+            TreeIterator::left(&position);
+    }
+
+    printf("Is it %s?\n", Tree::getOutFunc(&akinator.data)(bufN, TreeIterator::getValue(&position)));
+    scanf("%s", input);
+
+    if (*input == 'y')
+        printf("Yeeee\n");
+    else
+        printf("Nooo0o\n");
+
 
     if (Akinator::save(&akinator, "data.txt")) {
         printf("File error\n");
         return 1;
     }
+    logger::logMsg(COLORED_TEXT(COLORS_SETTINGS_NOTE_TEXTCOLOR, "end:"));
+    Tree__dump(akinator.data);
 
-    Akinator akinator2{};
-    Akinator__ctor(akinator2);
-
-    if (Akinator::upload(&akinator2, "data.txt")) {
-        printf("upload error\n");
-        return 1;
-    }
-
-    if (Akinator::save(&akinator2, "data2.txt")) {
-        printf("File error\n");
-        return 1;
-    }
-    Tree__dump(akinator2.data);
+    Tree::dtor(&akinator.data);
 
     printf("Program finished 0\n");
 
