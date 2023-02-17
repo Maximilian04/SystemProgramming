2d50930ae7e7f68cfaedc7dacd0728887518d778
Максимилиан <max4tim@bk.ru>
2023-02-17 03:02:37 +0300

    Winter23 DOS use LianLib

---
+++ Winter23/DOS/1602.23/Scn&Prt.asm
@@ -0,0 +1,49 @@
+.model tiny
+.code
+locals @@
+org 100h
+
+Start:
+                mov bx, 0d                      ; Scan number to bx
+                call ScanNDec
+                push bx
+
+                mov ax, 0b800h                  ; адрес сегмента с видеопамятью -> es
+                mov es, ax                      ;
+                mov dh, 00000010b               ; Dark green on black
+
+                pop ax                          ; Scanned number
+                push ax                         ;
+
+                mov bx, 160d*19 + 160d/2        ; Середина строчки
+
+                call PrintNDec
+
+                
+                pop ax                          ; Scanned number
+                push ax                         ;
+
+                mov bx, 160d*20 + 160d/2        ; Середина строчки
+
+                call PrintNHex
+
+                
+                pop ax                          ; Scanned number
+                push ax                         ;
+
+                mov bx, 160d*21 + 160d/2        ; Середина строчки
+
+                call PrintNBin
+
+                mov ax, 4c00h                   ; exit(0)
+                int 21h
+
+include ..\LianLib\ScanNDec.asm
+include ..\LianLib\PrntNBin.asm
+include ..\LianLib\PrntNHex.asm
+include ..\LianLib\PrntNDec.asm
+
+.data
+include ..\LianLib\Alphabet.asm
+
+end             Start
