8e330e1feebdc00872bbf079b8f5bb8f7fffc06b
Максимилиан <max4tim@bk.ru>
2023-04-07 03:36:50 +0300

    Winter23 ASM Return value fix

--- Winter23/NASM/0323.2023/hello.asm
+++ Winter23/NASM/0323.2023/hello.asm
@@ -21,41 +21,6 @@ section .text
 
 
 section .text
- 
-_start:
-    ; At _start the stack is 8 bytes misaligned because there is a return
-    ; address to the MSVCRT runtime library on the stack.
-    ; 8 bytes of temporary storage for `bytes`.
-    ; allocate 32 bytes of stack for shadow space.
-    ; 8 bytes for the 5th parameter of WriteConsole.
-    ; An additional 8 bytes for padding to make RSP 16 byte aligned.
-    sub     rsp, 8+8+8+32
-    ; At this point RSP is aligned on a 16 byte boundary and all necessary
-    ; space has been allocated.
- 
-    ; hStdOut = GetStdHandle(STD_OUTPUT_HANDLE)
-    mov     ecx, -11
-    call    GetStdHandle
- 
-    ; WriteFile(hstdOut, message, length(message), &bytes, 0);
-    mov     rcx, rax
-    mov     rdx, MsgText
-    mov     r8,  5
-    lea     r9,  [rsp-16]         ; Address for `bytes`
-    ; RSP-17 through RSP-48 are the 32 bytes of shadow space
-    mov     qword [rsp-56], 0     ; First stack parameter of WriteConsoleA function
-    call    WriteConsoleA
- 
-    ; ExitProcess(0)
-    ;    mov     rcx, 0
-    ;    call    ExitProcess
- 
-    ; alternatively you can exit by setting RAX to 0
-    ; and doing a ret
- 
-    add rsp, 8+8+32+8             ; Restore the stack pointer.
-    xor eax, eax                  ; RAX = return value = 0
-    ret
 
 
 __start:        
@@ -67,18 +32,16 @@ __start:
 
                 mov rbx, rax
 
-                mov rcx, rax            ; hConsoleOutput
+                mov rcx, 0
                 mov rdx, MsgText
-                mov r8, 4
+                mov r8, MsgTitle
                 mov r9, 0
-                push qword 0
-                call WriteConsoleA  ; WriteConsole (stdout, msg, 5, 0, 0)
-                pop rax
+                call MessageBoxA    ; MessageBoxA (NULL, MsgText, MsgTitle, 0)
 
                 mov rcx, rbx            ; hConsoleOutput
-                mov rdx, MsgText2
+                mov rdx, MsgText
                 mov r8, 5
-                mov r9, 4
+                mov r9, 0
                 push qword 0
                 call WriteConsoleA  ; WriteConsole (stdout, msg, 5, 0, 0)
                 pop rax
@@ -86,16 +49,20 @@ __start:
                 mov rcx, rbx            ; hConsoleOutput
                 mov rdx, MsgText3
                 mov r8, 5
-                mov r9, 9
+                mov r9, 0
                 push qword 0
                 call WriteConsoleA  ; WriteConsole (stdout, msg, 5, 0, 0)
                 pop rax
 
-                mov rcx, rbx
-                mov rdx, MsgText
-                mov r8, MsgTitle
+                mov rcx, rbx            ; hConsoleOutput
+                mov rdx, MsgText2
+                mov r8, 4
                 mov r9, 0
-                call MessageBoxA    ; MessageBoxA (NULL, MsgText, MsgTitle, 0)
+                push qword 0
+                call WriteConsoleA  ; WriteConsole (stdout, msg, 5, 0, 0)
+                pop rax
+
+
 
                 pop rbx
 
@@ -105,6 +72,6 @@ __start:
 section .data
 
 MsgTitle        db "Title", 0ah, 0
-MsgText         db "Text", 0
-MsgText2        db "Nooo",  'x', 0
+MsgText         db "Text", 0ah
+MsgText2        db "Noooo", 0
 MsgText3        db "Yess",  0ah, 0
